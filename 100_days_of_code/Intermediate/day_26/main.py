4ad27b52161ba3c50f0e5344b33dce7142db7c65
Tiago-S-Ribeiro <tiago.soaresribeiro@farfetch.com>
2022-01-06 12:48:35 +0000

    Day 26

--- 100_days_of_code/Intermediate/day_26/main.py
+++ 100_days_of_code/Intermediate/day_26/main.py
@@ -4,8 +4,15 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 phonetic_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
+input_invalid = True
 user_input = input("Insert a string:\n")
 
-codes = [phonetic_dict[letter.upper()] for letter in user_input]
-
-print(codes)
+while input_invalid:
+  try:
+    codes = [phonetic_dict[letter.upper()] for letter in user_input]
+    input_invalid = False
+  except KeyError:
+    print("Only strings allowed.\n")
+    user_input = input("Insert a string:\n")
+  else:
+    print(codes)
